0e7f0bedb25ea1b08405e5413b5869013ca2ad8f
Myron Marston <mmarston@whitepages.com>
2010-08-25 15:29:14 -0700

    Add is_url_valid?

--- lib/wp_validators.rb
+++ lib/wp_validators.rb
@@ -1,3 +1,13 @@
+require 'uri'
+
 module WpValidators
-  # Your code goes here...
+  def self.is_valid_url?(string)
+    uri = begin
+      URI::parse(string)
+    rescue URI::InvalidURIError, URI::BadURIError
+      return false
+    end
+
+    %w[http https].include?(uri.scheme)
+  end
 end
--- spec/wp_validators_spec.rb
+++ spec/wp_validators_spec.rb
@@ -1,8 +1,28 @@
 require 'spec_helper'
 
 describe WpValidators do
-  it "is a module" do
-    WpValidators.should be_a(Module)
+  describe '.is_valid_url?' do
+    def be_valid_url
+      simple_matcher("a valid url") do |str|
+        WpValidators.is_valid_url?(str)
+      end
+    end
+
+    it 'returns true for a valid http URL' do
+      'http://example.com/'.should be_valid_url
+    end
+
+    it 'returns true for a valid https URL' do
+      'https://example.com/'.should be_valid_url
+    end
+
+    it 'returns false for a string that is not a URL' do
+      'some string'.should_not be_valid_url
+    end
+
+    it 'returns false for an ftp URL' do
+      'ftp://example.com/'.should_not be_valid_url
+    end
   end
 end
 
